5057db4156dc5bba0898f08a278d2d9c329eb4cd
Mari-Couto <marinacoutonina@gmail.com>
2024-03-12 10:49:45 -0300

    delete inserido

--- routes/produtos.js
+++ routes/produtos.js
@@ -74,6 +74,31 @@ router.patch('/:id', (req, res) => {
     res.status(500).json({ error: 'Erro interno ao processar a requisição' });
   }
 });
+
+//deletar um produto
+router.delete('/:id', (req, res) => {
+  try {
+    const {id} = req.params;
+
+    const produtos = [
+      new Produto(1, 'Carrinho', 10.99, 'Descrição do produto 1'),
+      new Produto(2, 'Produto 2', 20.49, 'Descrição do produto 2'),
+      new Produto(3, 'Produto 3', 5.99, 'Descrição do produto 3')
+    ];
+    
+    const produtoIndex = produtos.findIndex(item => item.id === parseInt(id));
+    if(produtoIndex === -1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado."});
+    }
+
+    produtos.splice(produtoIndex, 1);
+
+    res.status(200).json({ mensagem: "Produto removido com sucesso"});
+  } catch (error) {
+    console.error('Erro ao processar a rota DELETE /:id', error);
+    res.status(500).json({ error: 'Erro interno ao processar a requisição' });
+  }
+});
   
 
 
