93baaef68307a05c6a9bd6fe7f9383875de941b5
Mari-Couto <marinacoutonina@gmail.com>
2024-03-13 13:26:38 -0300

    patch funcionando com o banco

--- routes/produtos.js
+++ routes/produtos.js
@@ -48,33 +48,38 @@ router.post('/', (req, res) => {
 
 
   //Alterar produto
-router.patch('/:id', (req, res) => {
-  try {
-    const { id } = req.params;
-    const { nome, preco, descricao } = req.body;
-
-    const osprodutos = [
-      new Produto(1, 'Carrinho', 10.99, 'Descrição do produto 1'),
-      new Produto(2, 'Produto 2', 20.49, 'Descrição do produto 2'),
-      new Produto(3, 'Produto 3', 5.99, 'Descrição do produto 3')
-    ];
-
-    const produtoIndex = osprodutos.findIndex(item => item.id === parseInt(id));
-    if (produtoIndex === -1) {
-      return res.status(404).json({ mensagem: "Produto não encontrado." });
+  router.patch('/:id', (req, res) => {
+ try {
+  mysql.query(
+     `UPDATE osprodutos
+      SET nome = ?,
+          preco = ?,
+          descricao = ?
+    WHERE id = ?`,
+ [req.body.nome, req.body.preco, req.body.descricao, req.params.id],
+   (err, result) => {
+     if (err) {
+     res.status(500).json({ error: 'Erro ao atualizar produto' });
+      } else {
+       if (result.affectedRows > 0) {
+         const produtoAtualizado = {
+           id: req.params.id,
+           nome: req.body.nome,
+           preco: req.body.preco,
+           descricao: req.body.descricao
+          };
+     res.status(200).json({ message: 'Produto atualizado com sucesso', produto: produtoAtualizado });
+      } else {
+     res.status(404).json({ error: 'Produto não encontrado' });
+      }}
+   }
+      );
+    } catch (error) {
+      console.error('Erro ao processar a rota PATCH /:id', error);
+      res.status(500).json({ error: 'Erro interno ao processar a requisição' });
     }
-
-    if (nome) osprodutos[produtoIndex].nome = nome;
-    if (preco) osprodutos[produtoIndex].preco = preco;
-    if (descricao) osprodutos[produtoIndex].descricao = descricao;
-
-    const produtoAtualizado = osprodutos[produtoIndex];
-    res.status(200).json({ mensagem: "Produto atualizado com sucesso", produto: produtoAtualizado });
-  } catch (error) {
-    console.error('Erro ao processar a rota PATCH /:id', error);
-    res.status(500).json({ error: 'Erro interno ao processar a requisição' });
-  }
-});
+  });
+  
 
 //deletar um produto
 router.delete('/:id', (req, res) => {
