8ef16ab4e98a60a25a4b19c1e7e57cf3e781e038
Mari-Couto <marinacoutonina@gmail.com>
2024-03-14 14:12:16 -0300

    get pedidos funcionando com banco

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const Pedido = require('../models/pedidoModelo');
+const mysql = require('../mysql')
 
 
 //exibir pedidos
 router.get('/', (req, res) => {
-    res.status(200).send({
-        message: "get funcionando"
-    })
+    try {
+        mysql.query('SELECT * FROM ospedidos', (err, results) =>{
+            if(err) {
+                throw err;
+            }
+            const pedidos = results.map(item => new Pedido(item.produto_id, item.quantidade));
+            res.status(200).json(pedidos);
+        });
+        
+    } catch (error) {
+        console.error('Erro ao executar a consulta:', error);
+        res.status(500).json({ error: 'Erro interno ao processar a requisição'})
+    }
 });
-
+ 
 // Inserir um pedido
 router.post('/', (req, res) => {
     res.status(201).send({
         message: "post funcionando"
     })
 });
+
 
 // alterar pedido
 router.patch('/', (req, res) =>{
