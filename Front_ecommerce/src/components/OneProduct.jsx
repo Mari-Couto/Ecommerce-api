91e7296137df0f91d3149711f0d4f97b57485957
Mari-Couto <marinacoutonina@gmail.com>
2024-04-23 10:24:51 -0300

    mensagem de id não encontrado em oneProduct

--- Front_ecommerce/src/components/OneProduct.jsx
+++ Front_ecommerce/src/components/OneProduct.jsx
@@ -4,13 +4,16 @@ import axios from 'axios';
 const OneProduct = () => {
   const [productId, setProductId] = useState('');
   const [product, setProduct] = useState(null);
+  const [message, setMessage] = useState('');
 
   const fetchProduct = async () => {
     try {
       const response = await axios.get(`http://localhost:3000/produtos/${productId}`);
-        setProduct(response.data[0]); 
-      } catch (error) {
+      setProduct(response.data[0]); 
+      setMessage('');
+    } catch (error) {
       console.error('Erro ao buscar produto:', error);
+      setMessage(`Produto com ID #${productId} não encontrado`);
       setProduct(null);
     }
   };
@@ -24,13 +27,16 @@ const OneProduct = () => {
         onChange={(e) => setProductId(e.target.value)}
       />
       <button onClick={fetchProduct}>Buscar Produto</button>
+      
+      {message && <p>{message}</p>}
+
       {product && (
         <div>
           <h2>{product.nome}</h2>
           <p>Id do produto: #{product.id}</p>
           <p>Descrição: {product.descricao}</p>
           <p>Preço: R$ {product.preco}</p>
-          {/* falta a imagem */}
+          {/* Falta a imagem */}
         </div>
       )}
     </div>
