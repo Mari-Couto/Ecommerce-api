304cbf1b96d6f4fc14f5d8a20e2db159c91646c5
Mari-Couto <marinacoutonina@gmail.com>
2024-04-23 14:17:26 -0300

    get de todos os produtos com imagem

--- Front_ecommerce/src/components/ProductList.jsx
+++ Front_ecommerce/src/components/ProductList.jsx
@@ -8,7 +8,23 @@ const ProductList = () => {
         async function fetchProducts() {
             try {
                 const res = await axios.get('http://localhost:3000/produtos');
-                setProducts(res.data);
+                const productsWithImages = await Promise.all(res.data.map(async product => {
+                    try {
+                        let imageUrl = null;
+                        if (product.file) {
+                            imageUrl = `http://localhost:3000/produtos/imagem/${product.id}`;
+                        }
+
+                        return {
+                            ...product,
+                            imageUrl: imageUrl
+                        };
+                    } catch (error) {
+                        console.error('Erro ao obter imagem do produto:', error);
+                        return product;
+                    }
+                }));
+                setProducts(productsWithImages);
             } catch (error) {
                 console.error('Erro ao obter os produtos:', error);
             }
@@ -16,23 +32,22 @@ const ProductList = () => {
         fetchProducts();
     }, []);
 
-  return (
-    <div>
-      <h1>Lista de produtos</h1>
-      <ul>
-               {products.map(product => (
+    return (
+        <div>
+            <h1>Lista de produtos</h1>
+            <ul>
+                {products.map(product => (
                     <li key={product.id}>
                         <h2>{product.nome}</h2>
                         <p>Id do produto: #{product.id}</p>
                         <p>Preço: R${product.preco}</p>
                         <p>Descrição: {product.descricao}</p>
-                               {/*Falta a imagem*/}
-                        
-            </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+                        {product.imageUrl && <img src={product.imageUrl} alt={product.nome} />}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ProductList;
